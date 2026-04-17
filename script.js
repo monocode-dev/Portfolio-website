ec553f74b2af1ae59e072266b8f6b2377fcc7f35
monocode <nothingfr2@gmail.com>
2026-04-17 18:50:15 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 const nav = document.querySelector("nav");
+const button = document.querySelector("#hamburger");
+const li = document.querySelectorAll("li")
+
+let open = false;
 
 window.addEventListener("scroll", () => {
     const scroll = window.scrollY;
     const opacity = Math.max(0, 1 - scroll / 400);
     nav.style.background = `rgba(32, 32, 32,${opacity})`;;
-            });
+});
+
+
+button.addEventListener("click", event =>{
+    if(!open){
+        event.target.textContent = "✖";
+        nav.classList.add("hamburger");
+        li.forEach(item =>{
+            item.style.display = "block";
+            item.style.margin = "15px 0px"
+        })
+        open = true;
+    }else{
+        event.target.textContent = "☰"
+        nav.classList.remove("hamburger")
+        li.forEach(item =>{
+            item.style.display = "none"
+        })
+        open = false;
+    }
+});
